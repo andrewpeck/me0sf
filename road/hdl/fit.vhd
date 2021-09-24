cd652995aeb13007dcc620185036bbb09bda1a0b
Andrew Peck <andrew.peck@cern.ch>
2021-09-24 11:43:03 -0400

    fit: use integers instead of signed
    simpler...

--- road/hdl/fit.vhd
+++ road/hdl/fit.vhd
@@ -16,8 +16,6 @@ use work.reciprocal_pkg.all;
 entity fit is
   generic(
 
-    DIVIDER : string := "FP";
-
     N_LAYERS : natural := 6;
 
     N_STAGES : natural := 8;
@@ -79,53 +77,43 @@ architecture behavioral of fit is
   -- s1
   --------------------------------------------------------------------------------
 
-  -- add 1 just to make everything signed...
-  constant LY_BITS : natural := 4;      -- 1 + integer(ceil(log2(real(N_LAYERS))));
-
   type cnt_array_t is array (integer range 0 to 7) of
-    signed(LY_BITS-1 downto 0);         -- number of layers hit
-  signal cnt : cnt_array_t := (others => to_signed(6, LY_BITS));
+    integer range 0 to 6;         -- number of layers hit
+  signal cnt : cnt_array_t := (others => 6);
 
-  constant NUM_EXTRA_SUM_BITS : natural
-    := integer(ceil(log2(real(N_LAYERS))));
+  type x_sum_array_t is array (integer range 1 to 5) of integer range 0 to 15; -- (min=0, max=0+1+2+3+4+5)
+  signal x_sum : x_sum_array_t := (others => 0);  -- sum (x_i)
 
-  type x_sum_array_t is array (integer range 1 to 5) of
-    signed (NUM_EXTRA_SUM_BITS+LY_BITS-1 downto 0);
-  type y_sum_array_t is array (integer range 1 to 6) of
-    signed (NUM_EXTRA_SUM_BITS+STRIP_BITS-1 downto 0);
-  signal x_sum : x_sum_array_t := (others => (others => '0'));  -- sum (x_i); need extra 3 bits for sum
-  signal y_sum : y_sum_array_t := (others => (others => '0'));  -- sum (y_i); need extra 3 bits for sum
+  -- since tracks are designed to go through the center, the positive and negative
+  -- will mostly offset and the sum will mostly be a small number, so the range can be restricted
+  -- the simulator will barf if we exceed it
+  type y_sum_array_t is array (integer range 1 to 6) of integer range -63 to 63;
+  signal y_sum : y_sum_array_t := (others => 0);  -- sum (y_i)
 
   -- n * x
-  type n_y_array_t is array (integer range 0 to N_LAYERS-1) of
-    signed (cnt(1)'length+ly(0)'length-1 downto 0);
-  type n_x_array_t is array (integer range 0 to N_LAYERS-1) of
-    signed (cnt(1)'length+NUM_EXTRA_SUM_BITS downto 0);
-  signal n_y : n_y_array_t := (others => (others => '0'));
-  signal n_x : n_x_array_t := (others => (others => '1'));
+  type n_x_array_t is array (integer range 0 to N_LAYERS-1) of integer range 0 to 5*6; -- ly=5 * cnt=6
+  type n_y_array_t is array (integer range 0 to N_LAYERS-1) of integer range -32*6+1 to 32*6-1;
+  signal n_x : n_x_array_t := (others => 1);
+  signal n_y : n_y_array_t := (others => 0);
 
   --------------------------------------------------------------------------------
   -- s2
   --------------------------------------------------------------------------------
 
-  type x_diff_array_t is array (integer range 0 to N_LAYERS-1) of
-    signed (NUM_EXTRA_SUM_BITS+1+LY_BITS-1 downto 0);
-  type y_diff_array_t is array (integer range 0 to N_LAYERS-1) of
-    signed (cnt(1)'length+ly(0)'length-1 downto 0);
-  signal y_diff : y_diff_array_t := (others => (others => '0'));  -- (y - mean(y))
-  signal x_diff : x_diff_array_t := (others => (others => '1'));  -- (x - mean(x))
+  type x_diff_array_t is array (integer range 0 to N_LAYERS-1) of integer range -15 to 15;
+  type y_diff_array_t is array (integer range 0 to N_LAYERS-1) of integer range -32*6+1 to 32*6-1;
+  signal x_diff : x_diff_array_t := (others => 1);  -- (x - mean(x))
+  signal y_diff : y_diff_array_t := (others => 0);  -- (y - mean(y))
 
   --------------------------------------------------------------------------------
   -- s3
   --------------------------------------------------------------------------------
 
-  type product_array_t is array (integer range 0 to N_LAYERS-1) of
-    signed (x_diff(0)'length + y_diff(0)'length-1 downto 0);
-  type square_array_t is array (integer range 0 to N_LAYERS-1) of
-    signed (2*x_diff(0)'length-1 downto 0);
+  type square_array_t is array (integer range 0 to N_LAYERS-1) of integer range 0 to 255;
+  type product_array_t is array (integer range 0 to N_LAYERS-1) of integer range -4095 to 4095;
 
-  signal product : product_array_t := (others => (others => '0'));  -- (x - mean(x)) * (y - mean(y))
-  signal square  : square_array_t  := (others => (others => '0'));  -- (x - mean(x)) ** 2
+  signal product : product_array_t := (others => 0);  -- (x - mean(x)) * (y - mean(y))
+  signal square  : square_array_t  := (others => 0);  -- (x - mean(x)) ** 2
 
   --------------------------------------------------------------------------------
   -- s4
@@ -133,8 +121,7 @@ architecture behavioral of fit is
 
   -- Σ (n*xi - Σx)*(n*yi - Σy)
   --
-  -- experimentally derived # of bits... should do something more comprehensive
-  signal product_sum : signed (13 downto 0) := (others => '0');
+  signal product_sum : integer range -8191 to 8191 := 0;
 
   -- Σ (n*xi - Σx)^2
   --
@@ -148,7 +135,6 @@ architecture behavioral of fit is
   -- initialize to 1 to prevent a divide by zero in simulation
   --
   signal square_sum : integer range 0 to 630 := 1;
-  --signal square_sum : signed (10 downto 0) := (others => '1');
 
   --------------------------------------------------------------------------------
   -- s5
@@ -165,7 +151,7 @@ architecture behavioral of fit is
   --------------------------------------------------------------------------------
 
   signal slope_times_x : sfixed
-    (M_INT_BITS + x_sum(1)'length-1 downto - M_FRAC_BITS+1) := (others => '0');
+    (M_INT_BITS + 11 -1 downto - M_FRAC_BITS+1) := (others => '0');
 
   --------------------------------------------------------------------------------
   -- s7
@@ -186,18 +172,17 @@ architecture behavioral of fit is
   --------------------------------------------------------------------------------
 
   -- sum 6 signed numbers with an enable for each number
-  function sum6 (p0    : signed;
-                 p1    : signed;
-                 p2    : signed;
-                 p3    : signed;
-                 p4    : signed;
-                 p5    : signed;
-                 en    : std_logic_vector (5 downto 0);
-                 nbits : natural)
-    return signed is
-    variable result : signed (nbits-1 downto 0);
+  function sum6 (p0    : integer;
+                 p1    : integer;
+                 p2    : integer;
+                 p3    : integer;
+                 p4    : integer;
+                 p5    : integer;
+                 en    : std_logic_vector (5 downto 0))
+    return integer is
+    variable result : integer;
   begin
-    result := (others => '0');
+    result := 0;
     if ('1' = en(0)) then
       result := result + p0;
     end if;
@@ -216,6 +201,7 @@ architecture behavioral of fit is
     if ('1' = en(5)) then
       result := result + p5;
     end if;
+
     return result;
   end;
 
@@ -259,7 +245,7 @@ begin
   ly(4) <= ly4;
   ly(5) <= ly5;
 
-  cnt(0) <= to_signed(zero_to_one(count_ones(valid_i)), LY_BITS);
+  cnt(0) <= zero_to_one(count_ones(valid_i));
 
   process (clock) is
   begin
@@ -283,24 +269,29 @@ begin
 
       -- Σx, Σy
 
-      y_sum(1) <= resize(sum6(ly(0), ly(1), ly(2), ly(3), ly(4), ly(5), valid_i,
-                              y_sum(1)'length), y_sum(1)'length);
-
-      x_sum(1) <= resize(sum6(x"0", x"1", x"2", x"3", x"4", x"5", valid_i,
-                              x_sum(1)'length), x_sum(1)'length);
+      y_sum(1) <= sum6(to_integer(ly(0)), to_integer(ly(1)), to_integer(ly(2)),
+                       to_integer(ly(3)), to_integer(ly(4)), to_integer(ly(5)), valid_i);
+      x_sum(1) <= sum6(0,1,2,3,4,5, valid_i);
 
       -- n * y_i
       -- n * x_i
       n_xy_loop : for I in 0 to N_LAYERS-1 loop
-        n_y(I) <= cnt(0) * ly(I);
-        n_x(I) <= cnt(0) * to_signed(I, LY_BITS);
+        n_y(I) <= cnt(0) * to_integer(ly(I));
+        n_x(I) <= cnt(0) * I;
       end loop;
 
       -- delays
 
-      cnt_dly : for idly in cnt'low+1 to cnt'high loop
-        cnt(idly) <= cnt(idly-1);
-      end loop;
+      cnt(1) <= cnt(0);
+      cnt(2) <= cnt(1);
+      cnt(3) <= cnt(2);
+      cnt(4) <= cnt(3);
+      cnt(5) <= cnt(4);
+      cnt(6) <= cnt(5);
+      cnt(7) <= cnt(6);
+      -- cnt_dly : for idly in 2 to 7 loop
+      --   cnt(idly) <= cnt(idly-1);
+      -- end loop;
 
       x_sum_dly : for I in x_sum'low+1 to x_sum'high loop
         x_sum(I) <= x_sum(I-1);
@@ -337,24 +328,17 @@ begin
       --------------------------------------------------------------------------------
 
       -- Σ (n*xi - Σx)*(n*yi - Σy)
-      product_sum <= resize(
-        sum6(product(0), product(1),
-             product(2), product(3),
-             product(4), product(5),
-             valid(3), 18), product_sum'length);
+      product_sum <= sum6(product(0), product(1), product(2), product(3), product(4), product(5), valid(3));
 
       -- Σ (n*xi - Σx)^2
-      square_sum <= to_integer(sum6(square(0), square(1),
-                                    square(2), square(3),
-                                    square(4), square(5),
-                                    valid(3), 19));
+      square_sum <= sum6(square(0), square(1), square(2), square(3), square(4), square(5), valid(3));
 
 
       --------------------------------------------------------------------------------
       -- s5 slope= Σ (n*xi - Σx)*(n*yi - Σy) / Σ (n*xi - Σx)^2
       --------------------------------------------------------------------------------
 
-      slope <= resize (to_sfixed(product_sum, product_sum'length) * reciprocal(square_sum), slope);
+      slope <= resize (to_sfixed(product_sum, 14) * reciprocal(square_sum), slope);
       -- slope <= resize (to_sfixed(product_sum, product_sum'length) /
       --                  to_sfixed(square_sum, square_sum'length), slope);
 
@@ -362,14 +346,14 @@ begin
       -- s6 slope*Σx
       --------------------------------------------------------------------------------
 
-      slope_times_x <= resize(slope * to_sfixed(x_sum(5), x_sum(5)'length), slope_times_x);
+      slope_times_x <= resize(slope * to_sfixed(x_sum(5), 11), slope_times_x);
       slope_s6      <= slope;
 
       --------------------------------------------------------------------------------
       -- s7 Σy-mb = Σy - slope*Σx
       --------------------------------------------------------------------------------
 
-      y_minus_mb <= resize((to_sfixed(y_sum(6), y_sum(6)'length) - slope_times_x), y_minus_mb);
+      y_minus_mb <= resize((to_sfixed(y_sum(6), 14) - slope_times_x), y_minus_mb);
       slope_s7   <= slope_s6;
 
       --------------------------------------------------------------------------------
@@ -381,70 +365,6 @@ begin
 
     end if;
   end process;
-
-  -- div_gen : if (DIVIDER="IP") generate
-  --   -- https://www.xilinx.com/html_docs/ip_docs/pru_files/div-gen.html
-  --   U0 : div_gen_v5_1_17
-  --     generic map (
-
-  --       -- General Options
-  --       C_XDEVICEFAMILY => "virtexuplus",
-  --       C_HAS_ARESETN   => 0,
-  --       C_HAS_ACLKEN    => 0,
-  --       C_LATENCY       => 4,
-  --       ALGORITHM_TYPE  => 3,             -- 3=High Radix
-
-  --       -- Widths
-  --       DIVISOR_WIDTH    => square_sum'length,
-  --       DIVIDEND_WIDTH   => product_sum'length,
-  --       FRACTIONAL_B     => 1,
-  --       FRACTIONAL_WIDTH => 10,
-
-  --       -- Signed vs. Unsigned
-  --       SIGNED_B => 1,
-
-  --       DIVCLK_SEL        => 1,           --
-  --       C_HAS_DIV_BY_ZERO => 0,
-  --       C_THROTTLE_SCHEME => 3,
-
-  --       -- Misc AXIS Settings
-  --       C_TLAST_RESOLUTION            => 0,
-  --       C_HAS_S_AXIS_DIVISOR_TUSER    => 0,
-  --       C_HAS_S_AXIS_DIVISOR_TLAST    => 0,
-  --       C_S_AXIS_DIVISOR_TUSER_WIDTH  => 1,
-  --       C_HAS_S_AXIS_DIVIDEND_TUSER   => 0,
-  --       C_HAS_S_AXIS_DIVIDEND_TLAST   => 0,
-  --       C_S_AXIS_DIVIDEND_TUSER_WIDTH => 1,
-  --       C_M_AXIS_DOUT_TUSER_WIDTH     => 1,
-
-  --       -- Data widths
-  --       C_S_AXIS_DIVISOR_TDATA_WIDTH  => 16,
-  --       C_S_AXIS_DIVIDEND_TDATA_WIDTH => 16,
-  --       C_M_AXIS_DOUT_TDATA_WIDTH     => 32
-  --       )
-  --     port map (
-  --       aclk                   => aclk,
-  --       aclken                 => '1',
-  --       aresetn                => '1',
-
-  --       -- unused axis settings
-  --       s_axis_divisor_tvalid  => '1',
-  --       s_axis_dividend_tvalid => '1',
-  --       s_axis_divisor_tready  => open,
-  --       s_axis_dividend_tready => open,
-  --       s_axis_divisor_tuser   => std_logic_vector(to_unsigned(0, 1)),
-  --       s_axis_divisor_tlast   => '0',
-  --       s_axis_dividend_tuser  => std_logic_vector(to_unsigned(0, 1)),
-  --       s_axis_dividend_tlast  => '0',
-  --       m_axis_dout_tready     => '0',
-  --       m_axis_dout_tvalid     => open,
-
-  --       -- unused axis settings
-  --       s_axis_divisor_tdata   => square_sum,
-  --       s_axis_dividend_tdata  => product_sum,
-  --       m_axis_dout_tdata      => slope
-  --       );
-  -- end generate;
 
   -- truncation, don't need to register
   intercept_o <= intercept(B_INT_BITS-1 downto -B_FRAC_BITS);
