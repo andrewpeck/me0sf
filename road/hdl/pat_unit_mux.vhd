db023d7cb5a3f9a191107d43b4aeed90155c8c6d
Andrew Peck <andrew.peck@cern.ch>
2021-10-04 16:57:58 -0400

    pat_unit: fix dav to phase instantation

--- road/hdl/pat_unit_mux.vhd
+++ road/hdl/pat_unit_mux.vhd
@@ -97,11 +97,11 @@ begin
   --------------------------------------------------------------------------------
 
   dav_to_phase_i_inst : entity work.dav_to_phase
-    generic map (MAX => MUX_FACTOR)
+    generic map (DIV => 8/MUX_FACTOR)
     port map (clock  => clock, dav => dav_i, phase_o => phase_i);
 
   dav_to_phase_o_inst : entity work.dav_to_phase
-    generic map (MAX => MUX_FACTOR)
+    generic map (DIV => 8/MUX_FACTOR)
     port map (clock  => clock, dav => patterns_mux_dav, phase_o => patterns_mux_phase);
 
   patgen : for I in 0 to NUM_SECTORS-1 generate
