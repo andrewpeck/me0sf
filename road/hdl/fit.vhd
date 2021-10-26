d8bc3e76f0a150052651d8fe033add60fb2cf39c
Andrew Peck <andrew.peck@cern.ch>
2021-10-26 18:17:04 -0400

    fit: use 'low instead of hardcoded 13

--- road/hdl/fit.vhd
+++ road/hdl/fit.vhd
@@ -348,7 +348,7 @@ begin
       square_sum_reciprocal <= reciprocal (
         sum6(square(0), square(1), square(2),
              square(3), square(4), square(5), valid(3)),
-        13);
+        -square_sum_reciprocal'low);
 
 
       --------------------------------------------------------------------------------
