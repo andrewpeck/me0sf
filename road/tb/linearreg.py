fed852d78ed480fd6346e2c0d04c3344a420f318
Chloe Grubb <cfgrubb@bu.edu>
2021-11-02 14:55:27 -0400

    update files for fit

--- road/tb/linearreg.py
+++ road/tb/linearreg.py
@@ -1,34 +1,187 @@
-#Two samples of the linear regression software
 import matplotlib.pyplot as plt
 import random
+from datadev import datadev
+from pat_unit_beh import process_pat
+from printly_dat import printly_dat
+from subfunc import *
+import statistics as stat
 
-#standard excel-esque style; uses linear least squares method and lists of data values
-def best_fit(x_data,y_data):
-    sum_x=sum(x_data)
-    sum_y=sum(y_data)
-    n=len(x_data)
-    xy_list=[]
-    x_sq_list=[]
+
+# FIX ME: NEED TO GET RID OF THE FANCY BITWISE ANDING OF DATA BEFORE FINDING CENTROID
+# WE CAN ASSUME THAT WE WILL FEED IN DATA VALUES THAT ARE ALREADY WITHIN THE MASK IN QUESTION
+# FIX ME: CHANGE 'NONE' DESIGNATION FOR NO HITS TO 0
+
+
+def find_pos(dat_pos_bin):
+    """takes in a string detailing the hits within a mask and returns these hit positions"""
+    positions = []
+    count = 0
+    for i in range(len(dat_pos_bin)):
+        if dat_pos_bin[i] == "1":
+            positions.append(i)
+            count += 1
+    # set any layer without hits within the mask to None; will be used in future data filtering
+    if count == 0:
+        positions = None
+    return positions
+
+
+def find_centroid(pat_id, patlist, data, ly_index, lut=None, MAX_SPAN=37):
+    """takes a given pattern ID, patlist, MAX_SPAN, and the list of  data for each layer to determine the hits in the pattern closest to the 'true' centroids"""
+    # take pat_id and patlist to determine integer pattern
+    pattern = get_mypattern(pat_id, patlist)
+    # get bit masks representing each integer layer pattern
+    pattern = get_ly_mask(pattern)
+    # isolate the bitmask of the layer index specified
+    pattern_ly = pattern[ly_index]
+    # get the length of the pattern bit mask
+    bin_ly_pat = bin(pattern_ly)[2:].zfill(MAX_SPAN)
+    len_ly_pat = len(bin_ly_pat)
+    # pad the data if data and mask are not same length
+    bin_ly_dat = bin(data)[2:].zfill(MAX_SPAN)
+    indices_p = []
+    indices_d = []
+    for i in range(len(bin_ly_pat)):
+        if bin_ly_pat[i] == "1":
+            indices_p.append(i)
+        if bin_ly_dat == "1":
+            indices_d.append(i)
+    hi_index_p = max(indices_p)
+    hi_index_d = max(indices_d)
+    if hi_index_d > hi_index_p:
+        data = data << (MAX_SPAN - hi_index_p)
+    # return a string the length of the layer pattern's mask, representing the hits present in the layer
+    positions = data & pattern_ly
+    # ensure that the positions vector has the same dimension as the original mask
+    positions = bin(positions)[2:].zfill(len_ly_pat)
+    # determine the integer positions of the hits in the layer
+    ly_pos = find_pos(positions)
+    sum_ly_pos = 0
+    if ly_pos != None:
+        # average the different positions of hits within the layer to find the 'true' centroid of the data set
+        for l in range(len(ly_pos)):
+            sum_ly_pos = sum_ly_pos + ly_pos[l]
+        centroid = sum_ly_pos // len(ly_pos)
+        # determine which data value in the layer is closest to this 'true' centroid; set that as the centroid for the data
+        min_dist = 999
+        closest = 0
+        for i in range(len(ly_pos)):
+            dist = abs(ly_pos[i] - centroid)
+            if dist <= min_dist:
+                min_dist = dist
+                closest = ly_pos[i]
+        centroid = closest
+    else:
+        centroid = None
+    return centroid
+
+
+def find_centroids(pat_id, patlist, ly_data, lut=None, MAX_SPAN=37):
+    """takes a given pattern ID, patlist, MAX_SPAN, and the list of layer data for each layer to determine the hits in the pattern closest to the 'true' centroids"""
+    centroids = []
+    for i in range(len(ly_data)):
+        centroids.append(
+            find_centroid(
+                pat_id, patlist, ly_data[i], ly_index=i, lut=None, MAX_SPAN=37
+            )
+        )
+    return centroids
+
+
+def find_true_coordinates(centroids, pat_id, patlist, MAX_SPAN=37):
+    """takes in a list of centroids for all layers, pat_id, patlist, and MAX_SPAN to determine the centroid coordinates relative to the entire layer"""
+    center = MAX_SPAN // 2
+    y_list = []
+    pattern = get_mypattern(pat_id, patlist)
+    if centroids[0] != None:
+        y_list.append(centroids[0] + pattern.ly0.lo + center)
+    else:
+        y_list.append(None)
+    if centroids[1] != None:
+        y_list.append(centroids[1] + pattern.ly1.lo + center)
+    else:
+        y_list.append(None)
+    if centroids[2] != None:
+        y_list.append(centroids[2] + pattern.ly2.lo + center)
+    else:
+        y_list.append(None)
+    if centroids[3] != None:
+        y_list.append(centroids[3] + pattern.ly3.lo + center)
+    else:
+        y_list.append(None)
+    if centroids[4] != None:
+        y_list.append(centroids[4] + pattern.ly4.lo + center)
+    else:
+        y_list.append(None)
+    if centroids[5] != None:
+        y_list.append(centroids[5] + pattern.ly5.lo + center)
+    else:
+        y_list.append(None)
+    return y_list
+
+
+def best_fit(x_data, y_data):
+    """takes in x_data and y_data to determine the values necessary for linear regression; uses linear least squares method"""
+    # eliminate data values that correlate to no hits within the mask of a layer
+    elimination_indices = []
+    for i in range(len(y_data)):
+        if y_data[i] == None:
+            elimination_indices.append(i)
+    elimination_vals = []
+    for j in range(len(elimination_indices)):
+        elimination_vals.append(x_data[elimination_indices[j]])
+    x_data = set(x_data)
+    elimination_vals = set(elimination_vals)
+    x_data = x_data - elimination_vals
+    x_data = list(x_data)
+    y_data = [n for n in y_data if n != None]
+    sum_x = 0
+    sum_y = 0
     for i in range(len(x_data)):
-        xy_list.append(x_data[i]*y_data[i])
-        x_sq_list.append(x_data[i]**2)
-    sum_xy=sum(xy_list)
-    sum_x_sq=sum(x_sq_list)
-    slope=(n*sum_xy-(sum_x*sum_y))/(n*sum_x_sq-sum_x**2)
-    b=(sum_y-slope*sum_x)/n
-    y_fit=[]
+        sum_x = sum_x + x_data[i]
+        sum_y = sum_y + y_data[i]
+    n = len(x_data)
+    xy_list = []
+    x_sq_list = []
+    for k in range(len(x_data)):
+        xy_list.append(x_data[k] * y_data[k])
+        x_sq_list.append(x_data[k] ** 2)
+    sum_xy = sum(xy_list)
+    sum_x_sq = sum(x_sq_list)
+    denominator = n * sum_x_sq - sum_x ** 2
+    if denominator == 0:
+        denominator = 1
+    slope = (n * sum_xy - (sum_x * sum_y)) / denominator
+    b = (sum_y - slope * sum_x) / n
+    y_fit = []
     for j in range(len(y_data)):
-        y_fit.append(slope*x_data[j]+b)
-    slope=round(slope,4)
-    b=round(b,4)
-    return slope, b, y_fit
+        y_fit.append(slope * x_data[j] + b)
+    slope = round(slope, 4)
+    b = round(b, 4)
 
-def print_my_best_fit(slope,b,x_data,y_data,y_fit):
-    string='y='+str(slope)+'+'+str(b)
-    plt.plot(x_data,y_data,'o')
-    plt.plot(x_data,y_fit,'k-',label=string)
-    plt.title('Sample Data')
+    return slope, b, y_fit, x_data, y_data
+
+
+def plot_my_best_fit(slope, b, x_data, y_data, y_fit):
+    """uses matplotlib.pyplot to plot line of best fit and print its equation to the figure"""
+    string = "y=" + str(slope) + "+" + str(b)
+    plt.ylim(1, 36)
+    plt.plot(x_data, y_data, "o")
+    plt.plot(x_data, y_fit, "k-", label=string)
+    plt.title("Sample Data")
     plt.legend()
     plt.show()
 
 
+# temporary testing framework
+# for i in range(15):
+#     [ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x] = datadev()
+#     ly_dat = [ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x]
+#     [pat_id, ly_c] = process_pat(
+#         patlist, ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN=37
+#     )
+#     centroids = find_centroids(pat_id, patlist, ly_dat, MAX_SPAN=37)
+#     x_list = [0, 1, 2, 3, 4, 5]
+#     y_list = find_true_coordinates(centroids, pat_id, patlist)
+#     [slope, b, y_fit, x_data, y_data] = best_fit(x_list, y_list)
+#     plot_my_best_fit(slope, b, x_data, y_data, y_fit)
