602c2e7afb505490e6d59f2e38cb1f5776dd5eaa
Jade Chismar <jchismar@bu.edu>
2022-06-16 12:27:38 -0400

    new mask class, modified patdef class in subfunc, cleanup

--- road/tb/pat_unit_beh.py
+++ road/tb/pat_unit_beh.py
@@ -2,26 +2,27 @@
 from subfunc import *
 from constants import *
 
+
+def shift_center(ly, MAX_SPAN=37):
+    """helper function to shift center of hi and lo values"""
+    center = round(MAX_SPAN/2)
+    hi = ly.hi + center
+    lo = ly.lo + center
+    return [lo, hi]
+
 def get_ly_mask(ly_pat, MAX_SPAN=37):
     """takes in a given layer pattern and returns a list of integer bit masks for each layer"""
     assert (
         type(ly_pat) == patdef_t
     ), "ly_pat input must be defined in the patdef_t class"
     assert (
-        type(ly_pat.ly0) == hi_lo_t
+        type(ly_pat.layers[0]) == hi_lo_t
     ), "each layer of ly_pat must be of the class hi_lo_t"
     assert type(ly_pat.id) == int, "ly_pat's id must be an integer"
     assert type(MAX_SPAN) == int, "MAX_SPAN input must be an integer"
     m_vec = []
-    center = round(MAX_SPAN / 2)
-    m_vals = []
-    #create list of pattern layers
-    ly_list = [ly_pat.ly0, ly_pat.ly1, ly_pat.ly2, ly_pat.ly3, ly_pat.ly4, ly_pat.ly5] 
     #for each layer, shift the provided hi and lo values for each layer from pattern definition by center
-    for layer in ly_list: 
-        hi = layer.hi + center
-        lo = layer.lo + center
-        m_vals.append([lo, hi])
+    m_vals = list(map(shift_center, ly_pat.layers))    
     # use the high and low indices to determine where the high bits must go for each layer
     for i in range(len(m_vals)):
         high_bits = 0
@@ -30,40 +31,41 @@ def get_ly_mask(ly_pat, MAX_SPAN=37):
             val = 1 << index
             high_bits = high_bits | val
         m_vec.append(high_bits)
-    return m_vec
+    mask_vec = Mask(m_vec, ly_pat.id)
+    return mask_vec
 
 def test_get_ly_mask():
     """ test function for get_ly_mask """
-    assert get_ly_mask(pat_straight) ==[0b11100000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b1000000000000000000, 0b11100000000000000000, 0b11100000000000000000]
-    assert get_ly_mask(pat_l) ==[0b111100000000000000, 0b1111000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b1111000000000000000000, 0b11110000000000000000000]
-    assert get_ly_mask(pat_r) == [0b11110000000000000000000, 0b1111000000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b1111000000000000000, 0b111100000000000000]
-    assert get_ly_mask(pat_l2) == [0b11110000000000000, 0b11111100000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b11110000000000000000000, 0b111100000000000000000000] 
-    assert get_ly_mask(pat_r2) == [0b111100000000000000000000, 0b11111100000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b111100000000000000, 0b11110000000000000]
-    assert get_ly_mask(pat_l3) == [0b11110000000000, 0b111100000000000, 0b1111000000000000000, 0b111110000000000000000, 0b11110000000000000000000000, 0b111100000000000000000000000]
-    assert get_ly_mask(pat_r3) == [0b111100000000000000000000000, 0b11110000000000000000000000, 0b1111000000000000000000, 0b111110000000000000000, 0b111100000000000, 0b11110000000000]
-    assert get_ly_mask(pat_l4) == [0b11110000000000, 0b111100000000000, 0b1111000000000000000, 0b111110000000000000000, 0b11110000000000000000000000,
- 0b111100000000000000000000000] 
+    assert get_ly_mask(pat_straight).mask ==[0b11100000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b1000000000000000000, 0b11100000000000000000, 0b11100000000000000000]
+    assert get_ly_mask(pat_l).mask ==[0b111100000000000000, 0b1111000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b1111000000000000000000, 0b11110000000000000000000]
+    assert get_ly_mask(pat_r).mask == [0b11110000000000000000000, 0b1111000000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b1111000000000000000, 0b111100000000000000]
+    assert get_ly_mask(pat_l2).mask == [0b11110000000000000, 0b11111100000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b11110000000000000000000, 0b111100000000000000000000] 
+    assert get_ly_mask(pat_r2).mask == [0b111100000000000000000000, 0b11111100000000000000000, 0b11100000000000000000, 0b11100000000000000000, 0b111100000000000000, 0b11110000000000000]
+    assert get_ly_mask(pat_l3).mask == [0b11110000000000, 0b111100000000000, 0b1111000000000000000, 0b111110000000000000000, 0b11110000000000000000000000, 0b111100000000000000000000000]
+    assert get_ly_mask(pat_r3).mask == [0b111100000000000000000000000, 0b11110000000000000000000000, 0b1111000000000000000000, 0b111110000000000000000, 0b111100000000000, 0b11110000000000]
+    assert get_ly_mask(pat_l4).mask == [0b11110000000000, 0b111100000000000, 0b1111000000000000000, 0b111110000000000000000, 0b11110000000000000000000000, 0b111100000000000000000000000] 
 
-def get_lc_id(patlist, ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN):
-    """takes in patlist and the data from each layer to determine the layer count that would be obtained from the data overlaying any mask; returns id and layer count for each mask"""
-    corr_pat_id = []
-    pats_m = []
-    for w in range(len(patlist)):
-        pats_m.append(get_ly_mask(patlist[w], MAX_SPAN))
+def return_layer_mask():
+    """create layer masks for patterns in patlist"""
+    global LAYER_MASK
+    LAYER_MASK = list(map(get_ly_mask, patlist))
+    
+return_layer_mask()
+
+def get_lc_id(ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN=37):
+    """takes in the data from each layer to determine the layer count that would be obtained from the data overlaying any mask; returns id and layer count for each mask"""
     lc_vec_x = []
     lc_id_vec = []
     data = [ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x]
 
     # and the layer data with the respective mask layer to determine how many hits are in each layer
-    for v in range(len(patlist)):
+    for v in range(len(LAYER_MASK)):
         ly_hits = [] 
         for i in range(N_LAYERS):
-            ly_hits.append(data[i] & pats_m[v][i])
+            ly_hits.append(data[i] & LAYER_MASK[v].mask[i])
  
         # count the hits in each layer and store in ly_ones
-        ly_ones = []
-        for ly in ly_hits:
-            ly_ones.append(count_ones(ly))
+        ly_ones = list(map(count_ones, ly_hits))
  
         # assign a layer count of 1 for at least 1 hit in a mask for a given layer, else assign layer count of 0
         layer_count = []
@@ -76,25 +78,21 @@ def get_lc_id(patlist, ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN):
         # total up layer counts for each layer to get pattern's total layer count
         lc_vec_x.append(sum(layer_count))
 
-    #add layer id to corr_pat_id
-    for i in range(len(patlist)):
-        corr_pat_id.append(patlist[i].id)
-
     # match the correct pattern id to its total layer count
-    for p in range(len(patlist)):
-        lc_id_vec.append([lc_vec_x[p], corr_pat_id[p]])
+    for p in range(len(LAYER_MASK)):
+        lc_id_vec.append([lc_vec_x[p], LAYER_MASK[p].id])
 
     return lc_id_vec
 
 def test_get_lc_id():
     """ test function for get_lc_id """
-    assert get_lc_id(patlist, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 37) == [[6, 15], [4, 14], [4, 13], [3, 12], [3, 11], [2, 10], [2, 9], [2, 8], [2, 7], [2, 6], [2, 5], [1, 4], [1, 3], [1, 2], [1, 1]]
-    assert get_lc_id(patlist, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 37) == [[5, 15], [4, 14], [4, 13], [3, 12], [4, 11], [2, 10], [1, 9], [2, 8], [1, 7], [1, 6], [1, 5], [1, 4], [1, 3], [1, 2], [1, 1]]
-    assert get_lc_id(patlist, 0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[4, 15], [6, 14], [3, 13], [5, 12], [3, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [2, 5], [1, 4], [1, 3], [1, 2], [1, 1]]
-    assert get_lc_id(patlist, 0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[3, 15], [5, 14], [2, 13], [4, 12], [2, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
-    assert get_lc_id(patlist, 0b100000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[2, 15], [4, 14], [1, 13], [3, 12], [1, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
-    assert get_lc_id(patlist, 0b100000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[1, 15], [3, 14], [1, 13], [2, 12], [1, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
-    assert get_lc_id(patlist, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[0, 15], [2, 14], [2, 13], [2, 12], [2, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
+    assert get_lc_id(0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 37) == [[6, 15], [4, 14], [4, 13], [3, 12], [3, 11], [2, 10], [2, 9], [2, 8], [2, 7], [2, 6], [2, 5], [1, 4], [1, 3], [1, 2], [1, 1]]
+    assert get_lc_id(0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 37) == [[5, 15], [4, 14], [4, 13], [3, 12], [4, 11], [2, 10], [1, 9], [2, 8], [1, 7], [1, 6], [1, 5], [1, 4], [1, 3], [1, 2], [1, 1]]
+    assert get_lc_id(0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[4, 15], [6, 14], [3, 13], [5, 12], [3, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [2, 5], [1, 4], [1, 3], [1, 2], [1, 1]]
+    assert get_lc_id(0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[3, 15], [5, 14], [2, 13], [4, 12], [2, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
+    assert get_lc_id(0b100000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[2, 15], [4, 14], [1, 13], [3, 12], [1, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
+    assert get_lc_id(0b100000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[1, 15], [3, 14], [1, 13], [2, 12], [1, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
+    assert get_lc_id(0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == [[0, 15], [    2, 14], [2, 13], [2, 12], [2, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]
 
 def priority_encoder(lc_id_vec):
     """
@@ -140,14 +138,14 @@ def test_priority_encoder():
     assert priority_encoder([[0, 15], [2, 14], [2, 13], [2, 12], [2, 11], [1, 10], [2, 9], [1, 8], [2, 7], [1, 6], [1, 5], [1, 4], [1, 3], [2, 2], [1, 1]]) == (2, 14) 
 
 
-def process_pat(patlist, ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN=37):
+def process_pat(ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN=37):
 
     """
     takes in a list of patterns and sample data for each layer to generate a
     layer count and id for the pattern that best matches the data. Returns best id, layer count
     """
 
-    lc_id_vec = get_lc_id(patlist, ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN)
+    lc_id_vec = get_lc_id(ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN)
 
     [ly_c, pat_id] = priority_encoder(lc_id_vec)
     if ly_c == 0:
@@ -155,10 +153,10 @@ def process_pat(patlist, ly0_x, ly1_x, ly2_x, ly3_x, ly4_x, ly5_x, MAX_SPAN=37):
     return pat_id, ly_c
 
 def test_process_pat():
-    assert process_pat(patlist, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 37) == (15, 6)
-    assert process_pat(patlist, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 37) == (15, 5)
-    assert process_pat(patlist, 0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 6)
-    assert process_pat(patlist, 0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 5)
-    assert process_pat(patlist, 0b100000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 4)
-    assert process_pat(patlist, 0b100000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 3)
-    assert process_pat(patlist, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 2)
+    assert process_pat(0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 0b1000000000000000000, 37) == (15, 6)
+    assert process_pat(0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 0b100000000000000000, 37) == (15, 5)
+    assert process_pat(0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 6)
+    assert process_pat(0b100000000000000000, 0b1000000000000000000, 0b10000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 5)
+    assert process_pat(0b100000000000000000, 0b1000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 4)
+    assert process_pat(0b100000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 3)
+    assert process_pat(0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 0b100000000000000000000, 37 ) == (14, 2)
--- road/tb/subfunc.py
+++ road/tb/subfunc.py
@@ -8,41 +8,29 @@ class hi_lo_t:
 
 
 class patdef_t:
-    def __init__(self, id, ly0, ly1, ly2, ly3, ly4, ly5):
+    def __init__(self, id, layer_list):
         self.id = id
-        self.ly0 = ly0
-        self.ly1 = ly1
-        self.ly2 = ly2
-        self.ly3 = ly3
-        self.ly4 = ly4
-        self.ly5 = ly5
+        self.layers = layer_list
 
+class Mask:
+    def __init__(self, mask, id):
+        self.mask = mask
+        self.id = id
+
+def mirror_hi_lo(ly):
+    """"helper function for mirror_patdef, mirrors the hi and lo values"""
+    return hi_lo_t(ly.lo * (-1), ly.hi * (-1))
 
 def mirror_patdef(pat, id):
     """takes in a pattern definition and an id and returns a mirrored pattern definition associated with that id"""
     assert type(pat) == patdef_t, "pat input must be of the class patdef_t"
-    assert type(pat.ly0) == hi_lo_t, "each layer of pat must be of the class hi_lo_t"
+    assert type(pat.layers[0]) == hi_lo_t, "each layer of pat must be of the class hi_lo_t"
     assert type(id) == int, "id input must be an integer"
-    ly0_h = pat.ly0.lo * (-1)
-    ly0_l = pat.ly0.hi * (-1)
-    ly1_h = pat.ly1.lo * (-1)
-    ly1_l = pat.ly1.hi * (-1)
-    ly2_h = pat.ly2.lo * (-1)
-    ly2_l = pat.ly2.hi * (-1)
-    ly3_h = pat.ly3.lo * (-1)
-    ly3_l = pat.ly3.hi * (-1)
-    ly4_h = pat.ly4.lo * (-1)
-    ly4_l = pat.ly4.hi * (-1)
-    ly5_h = pat.ly5.lo * (-1)
-    ly5_l = pat.ly5.hi * (-1)
-    ly0 = hi_lo_t(ly0_h, ly0_l)
-    ly1 = hi_lo_t(ly1_h, ly1_l)
-    ly2 = hi_lo_t(ly2_h, ly2_l)
-    ly3 = hi_lo_t(ly3_h, ly3_l)
-    ly4 = hi_lo_t(ly4_h, ly4_l)
-    ly5 = hi_lo_t(ly5_h, ly5_l)
-    result = patdef_t(id, ly0, ly1, ly2, ly3, ly4, ly5)
-    return result
+
+    mirrored_ly = list(map(mirror_hi_lo, pat.layers))
+    mirrored_pat = patdef_t(id, mirrored_ly)
+    return mirrored_pat
+    
 
 
 # FIXME: take these globals out of here and bury them somewhere
@@ -50,81 +38,81 @@ def mirror_patdef(pat, id):
 # true patlist; only used for testing pat_unit.vhd emulator
 pat_straight = patdef_t(
     15,
-    hi_lo_t(1, -1),
+    [hi_lo_t(1, -1),
     hi_lo_t(1, -1),
     hi_lo_t(1, -1),
     hi_lo_t(0, 0),
     hi_lo_t(1, -1),
-    hi_lo_t(1, -1),
+    hi_lo_t(1, -1)]
 )
 pat_l = patdef_t(
     14,
-    hi_lo_t(-1, -4),
+    [hi_lo_t(-1, -4),
     hi_lo_t(0, -3),
     hi_lo_t(1, -1),
     hi_lo_t(1, -1),
     hi_lo_t(3, 0),
-    hi_lo_t(4, 1),
+    hi_lo_t(4, 1)]
 )
 pat_r = mirror_patdef(pat_l, pat_l.id - 1)
 pat_l2 = patdef_t(
     12,
-    hi_lo_t(-2, -5),
+    [hi_lo_t(-2, -5),
     hi_lo_t(1, -4),
     hi_lo_t(1, -1),
     hi_lo_t(1, -1),
     hi_lo_t(4, 1),
-    hi_lo_t(5, 2),
+    hi_lo_t(5, 2)]
 )
 pat_r2 = mirror_patdef(pat_l2, pat_l2.id - 1)
 pat_l3 = patdef_t(
     10,
-    hi_lo_t(-5, -8),
+    [hi_lo_t(-5, -8),
     hi_lo_t(-4, -7),
     hi_lo_t(0, -3),
     hi_lo_t(2, -2),
     hi_lo_t(7, 4),
-    hi_lo_t(8, 5),
+    hi_lo_t(8, 5)]
 )
 pat_r3 = mirror_patdef(pat_l3, pat_l3.id - 1)
 pat_l4 = patdef_t(
     8,
-    hi_lo_t(-5, -8),
+    [hi_lo_t(-5, -8),
     hi_lo_t(-4, -7),
     hi_lo_t(0, -3),
     hi_lo_t(2, -2),
     hi_lo_t(7, 4),
-    hi_lo_t(8, 5),
+    hi_lo_t(8, 5)]
 )
 pat_r4 = mirror_patdef(pat_l4, pat_l4.id - 1)
 pat_l5 = patdef_t(
     6,
-    hi_lo_t(-8, -11),
+    [hi_lo_t(-8, -11),
     hi_lo_t(-5, -9),
     hi_lo_t(0, -3),
     hi_lo_t(3, 0),
     hi_lo_t(9, 5),
-    hi_lo_t(11, 8),
+    hi_lo_t(11, 8)]
 )
 pat_r5 = mirror_patdef(pat_l5, pat_l5.id - 1)
 pat_l6 = patdef_t(
     4,
-    hi_lo_t(-11, -15),
+    [hi_lo_t(-11, -15),
     hi_lo_t(-9, -11),
     hi_lo_t(4, -9),
     hi_lo_t(9, 4),
     hi_lo_t(11, 9),
-    hi_lo_t(15, 11),
+    hi_lo_t(15, 11)]
 )
 pat_r6 = mirror_patdef(pat_l6, pat_l6.id - 1)
 pat_l7 = patdef_t(
     2,
-    hi_lo_t(-10, -18),
+    [hi_lo_t(-10, -18),
     hi_lo_t(-6, -14),
     hi_lo_t(2, -9),
     hi_lo_t(9, 2),
     hi_lo_t(14, 6),
-    hi_lo_t(18, 10),
+    hi_lo_t(18, 10)]
 )
 pat_r7 = mirror_patdef(pat_l7, pat_l7.id - 1)
 
@@ -186,50 +174,6 @@ def ones_bit_mask(num):
     return o_mask
 
 
-def get_ly_mask(ly_pat, MAX_SPAN=37):
-    """takes in a given layer pattern and returns a list of integer bit masks for each layer"""
-    assert (
-        type(ly_pat) == patdef_t
-    ), "ly_pat input must be defined in the patdef_t class"
-    assert (
-        type(ly_pat.ly0) == hi_lo_t
-    ), "each layer of ly_pat must be of the class hi_lo_t"
-    assert type(ly_pat.id) == int, "ly_pat's id must be an integer"
-    assert type(MAX_SPAN) == int, "MAX_SPAN input must be an integer"
-    m_vec = []
-    center = round(MAX_SPAN / 2)
-    # generate indices of the high bits for each layer based on the provided hi and lo values from the pattern definition
-    a_lo = ly_pat.ly0.lo + center
-    a_hi = ly_pat.ly0.hi + center
-    b_lo = ly_pat.ly1.lo + center
-    b_hi = ly_pat.ly1.hi + center
-    c_lo = ly_pat.ly2.lo + center
-    c_hi = ly_pat.ly2.hi + center
-    d_lo = ly_pat.ly3.lo + center
-    d_hi = ly_pat.ly3.hi + center
-    e_lo = ly_pat.ly4.lo + center
-    e_hi = ly_pat.ly4.hi + center
-    f_lo = ly_pat.ly5.lo + center
-    f_hi = ly_pat.ly5.hi + center
-    m_vals = [
-        [a_lo, a_hi],
-        [b_lo, b_hi],
-        [c_lo, c_hi],
-        [d_lo, d_hi],
-        [e_lo, e_hi],
-        [f_lo, f_hi],
-    ]
-    # use the high and low indices to determine where the high bits must go for each layer
-    for i in range(len(m_vals)):
-        holder = 0
-        # keep setting high bits from the low index to the high index; leave all else as low bits
-        for index in range(m_vals[i][0], m_vals[i][1] + 1):
-            val = 1 << index
-            holder = holder | val
-        m_vec.append(holder)
-    return m_vec
-
-
 def get_mypattern(pat_id: int, patlist: "List[patdef_t]") -> patdef_t:
     assert type(pat_id) == int, "pat_id input must be an integer"
     assert (
@@ -246,3 +190,4 @@ def get_mypattern(pat_id: int, patlist: "List[patdef_t]") -> patdef_t:
         if patlist[i].id == pat_id:
             mypattern = patlist[i]
     return mypattern
+
