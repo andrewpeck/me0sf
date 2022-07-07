0e3ebae010270be3b76ae497ad3c2a1430860b93
Jade Chismar <jchismar@bu.edu>
2022-07-07 12:49:26 -0400

    cleanup, add test cases

--- road/tb/partition_beh.py
+++ road/tb/partition_beh.py
@@ -1,12 +1,11 @@
 """ Emulator that processes a single partition (6 layers x 192 strips) and returns a collection of segments"""
 import numpy as np
 from pat_unit_mux_beh import pat_mux
-#from datadev_mux import datadev_mux
 from subfunc import *
 
 def compare_ghosts(seg, comp_list):
     """takes in a segment and a list of segments to ensure that there aren't copies of the same data (ID value identical) or mirrors (ID value +2 or -2 from each other)"""
-    comp_list = [x for x in comp_list if x.id != 0]
+    comp_list = [x for x in comp_list if x.id != 0 ]
     if len(comp_list) != 0:
         for i in range(len(comp_list)):
             if seg.id == 0 and seg.lc == 0:
@@ -16,27 +15,67 @@ def compare_ghosts(seg, comp_list):
                 or seg.id + 2 == comp_list[i].id
                 or seg.id - 2 == comp_list[i].id
             ):
-                seg.id = 0
-                seg.lc = 0
+                seg.reset()
     return seg
 
-def partition_filtering(pat_mux_dat, group_width=8, ghost_width=4, WIDTH=37):
-    """takes in pat_unit_mux_data and performs edge cancellation"""
-    for group in range(round((WIDTH - 1) / group_width)):
-        lo_index = int(group + group_width - (ghost_width / 2))  # maybe check this first if there's discrepancies
-        hi_index = lo_index + (ghost_width)
+def test_compare_ghosts():
+    seg_list = [Segment(6, 15), Segment(6, 12), Segment(6,5)]
+    seg1 = Segment(6, 15)
+    seg2 = Segment(6, 10)
+    seg3 = Segment(6, 7)
+    #check for reset with copy, ID+2, ID-2
+    assert compare_ghosts(seg1, seg_list).id == 0
+    assert compare_ghosts(seg2, seg_list).id == 0
+    assert compare_ghosts(seg3, seg_list).id == 0
+
+def cancel_edges(pat_mux_dat, group_width=8, ghost_width=4, WIDTH=192): 
+    """takes in pat_unit_mux_data, finds edges of groups w/given group width, and performs edge cancellation by checking ghosts around each edge within given ghost width"""
+    for edge in range((WIDTH // group_width)-1): 
+        lo_index = group_width*(edge+1) - (ghost_width//2)
+        hi_index = lo_index + ghost_width
         for j in range(lo_index, hi_index): 
             pat_mux_dat[j] = compare_ghosts(pat_mux_dat[j], pat_mux_dat[(j + 1):hi_index])
     return pat_mux_dat
 
+def test_cancel_edges():
+    seg_list1 = []
+    for i in range(24):
+        seg_list1.append(Segment(6, 15)) 
+    cancelled1 = cancel_edges(seg_list1, 8, 4, 24)
+    #check first edge is cancelled correctly
+    assert cancelled1[6].id == 0
+    assert cancelled1[7].id == 0
+    assert cancelled1[8].id == 0
+    assert cancelled1[9].id == 15
+    #check second edge is cancelled correctly 
+    assert cancelled1[14].id == 0 
+    assert cancelled1[15].id == 0
+    assert cancelled1[16].id == 0
+    assert cancelled1[17].id == 15
+    seg_list2 = [Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 14),Segment(6, 11), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15),Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15), Segment(6, 15)]
+    cancelled2 = cancel_edges(seg_list2, 8, 4, 24)
+    #check only 6th segment is cancelled in first edge
+    assert cancelled2[6].id == 0
+    assert cancelled2[7].id == 14
+    assert cancelled2[8].id == 11
+
 def work_partition(partition_data, MAX_SPAN=37, WIDTH=192, group_width=8, ghost_width=4):
-    """takes in pat_unit_mux_data, a group size, and a ghost width to return a smaller data set, using ghost edge cancellation
+    """takes in partition data, a group size, and a ghost width to return a smaller data set, using ghost edge cancellation
     and segment quality filtering
 
-    NOTE: ghost width denotes the width where we can likely see copies of the same segment in the data"""
-    #perform edge cancellation on pat_unit_mux_data
-    pat_mux_dat = np.array(partition_filtering(pat_mux(partition_data, MAX_SPAN, WIDTH), group_width, ghost_width, WIDTH))
+    NOTE: ghost width denotes the width where we can likely see copies of the same segment in the data
 
-    #divide partition into 24 pieces and take best segment from each piece
+    steps: process partition data with pat_mux, perfom edge cancellations, divide partition into pieces, take best segment from each piece"""
+    #process the data with pat_mux and perform edge cancellation 
+    pat_mux_dat = np.array(cancel_edges(pat_mux(partition_data, MAX_SPAN, WIDTH), group_width, ghost_width, WIDTH))
+    #divide partition into pieces and take best segment from each piece
     final_dat = list(map(max, pat_mux_dat.reshape(WIDTH//group_width, group_width)))
     return final_dat
+
+def test_work_partition():
+    data = [0b1, 0b1, 0b1, 0b1, 0b1, 0b1]
+    part = work_partition(data)
+    assert part[0].id == 15
+    assert part[0].lc == 6
+    assert part[1].id == 0
+    assert part[1].lc == 0
