e7f897d12aa18cc677fe321ff99e42f93316c7cb
Andrew Peck <andrew.peck@cern.ch>
2023-02-16 14:15:30 -0500

    Add partition latency to pat_pkg

--- road/hdl/pat_pkg.vhd
+++ road/hdl/pat_pkg.vhd
@@ -19,6 +19,7 @@ package pat_pkg is
 
   constant PAT_UNIT_LATENCY     : positive := 5;
   constant PAT_UNIT_MUX_LATENCY : positive := PAT_UNIT_LATENCY + 3;
+  constant PARTITION_LATENCY    : positive := PAT_UNIT_MUX_LATENCY + 5;
 
   type chamber_t is array(integer range 0 to 7) of partition_t;
 
