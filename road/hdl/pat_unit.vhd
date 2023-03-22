172392c2ac8f710849252d893816b0fa3e8c82a0
Andrew Peck <andrew.peck@cern.ch>
2023-03-22 11:00:19 -0400

    ignore strip and partition in pat_unit sort

--- road/hdl/pat_unit.vhd
+++ road/hdl/pat_unit.vhd
@@ -253,13 +253,14 @@ begin
 
   priority_encoder_inst : entity work.priority_encoder
     generic map (
-      WIDTH      => NUM_PATTERNS,
-      REG_INPUT  => true,
-      REG_OUTPUT => true,
-      REG_STAGES => 2,
-      DAT_BITS   => best_slv'length,
-      QLT_BITS   => PATTERN_SORTB,
-      ADR_BITS_o => integer(ceil(log2(real(NUM_PATTERNS))))
+      WIDTH       => NUM_PATTERNS,
+      REG_INPUT   => true,
+      REG_OUTPUT  => true,
+      REG_STAGES  => 2,
+      DAT_BITS    => best_slv'length,
+      QLT_BITS    => PATTERN_SORTB,
+      IGNORE_BITS => STRIP_BITS + PARTITION_BITS,
+      ADR_BITS_o  => integer(ceil(log2(real(NUM_PATTERNS))))
       )
     port map (
       clock => clock,
