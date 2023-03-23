95eee6ebf9dec6612a89bca37307789eb1eab567
Andrew Peck <andrew.peck@cern.ch>
2023-03-23 13:24:15 -0400

    refactor x-partition cancellation into a function

--- road/tb/chamber_beh.py
+++ road/tb/chamber_beh.py
@@ -5,6 +5,53 @@ import operator
 from partition_beh import process_partition
 import math
 
+def cross_partition_cancellation(segments):
+
+    for i in range(1,15,2):
+
+        for (l,seg) in enumerate(segments[i]):
+            if seg.id == 0:
+                continue
+            #print ("Seg 0: ", l, seg, seg.strip)
+            strip = seg.strip
+            #quality = seg.quality
+            seg1_max_quality = -9999
+            seg2_max_quality = -9999
+            seg1_max_quality_index = -9999
+            seg2_max_quality_index = -9999
+
+            for (j,seg1) in enumerate(segments[i-1]):
+                if seg1.id == 0:
+                    continue
+                #print ("Seg 1: ", l, seg1, seg1.strip)
+                if abs(strip - seg1.strip) <= cross_part_seg_width:
+                    if seg1.quality > seg1_max_quality:
+                        if seg1_max_quality_index != -9999:
+                            segments[i-1][seg1_max_quality_index].reset()
+                        seg1_max_quality_index = j
+                        seg1_max_quality = seg1.quality
+
+            for (k,seg2) in enumerate(segments[i+1]):
+                if seg2.id == 0:
+                    continue
+                #print ("Seg 2: ", l, seg2, seg2.strip)
+                if abs(strip - seg2.strip) <= cross_part_seg_width:
+                    if seg2.quality > seg2_max_quality:
+                        if seg2_max_quality_index != -9999:
+                            segments[i+1][seg2_max_quality_index].reset()
+                        seg2_max_quality_index = k
+                        seg2_max_quality = seg2.quality
+
+            if seg1_max_quality_index != -9999 and seg2_max_quality_index != -9999:
+                segments[i-1][seg1_max_quality_index].reset()
+                segments[i+1][seg2_max_quality_index].reset()
+            elif seg1_max_quality_index != -9999:
+                segments[i][l].reset()
+            elif seg2_max_quality_index != -9999:
+                segments[i][l].reset()
+
+    return segments
+
 def process_chamber(chamber_data,
                     hit_thresh : int = 4,
                     ly_thresh : int = 4,
@@ -39,76 +86,7 @@ def process_chamber(chamber_data,
 
     # Remove redundant segments from cross-partitions and grouping neighbouring eta partitions
     if (cross_part_seg_width > 0):
-        for i in range(1,15,2):
-            #print (i)
-            for (l,seg) in enumerate(segments[i]):
-                if seg.id == 0:
-                    continue
-                #print ("Seg 0: ", l, seg, seg.strip)
-                strip = seg.strip
-                #quality = seg.quality
-                seg1_max_quality = -9999
-                seg2_max_quality = -9999
-                seg1_max_quality_index = -9999
-                seg2_max_quality_index = -9999
-                for (j,seg1) in enumerate(segments[i-1]):
-                    if seg1.id == 0:
-                        continue
-                    #print ("Seg 1: ", l, seg1, seg1.strip)
-                    if abs(strip - seg1.strip) <= cross_part_seg_width:
-                        if seg1.quality > seg1_max_quality:
-                            if seg1_max_quality_index != -9999:
-                                segments[i-1][seg1_max_quality_index].reset()
-                            seg1_max_quality_index = j
-                            seg1_max_quality = seg1.quality
-                for (k,seg2) in enumerate(segments[i+1]):
-                    if seg2.id == 0:
-                        continue
-                    #print ("Seg 2: ", l, seg2, seg2.strip)
-                    if abs(strip - seg2.strip) <= cross_part_seg_width:
-                        if seg2.quality > seg2_max_quality:
-                            if seg2_max_quality_index != -9999:
-                                segments[i+1][seg2_max_quality_index].reset()
-                            seg2_max_quality_index = k
-                            seg2_max_quality = seg2.quality
-
-                if seg1_max_quality_index != -9999 and seg2_max_quality_index != -9999:
-                    segments[i-1][seg1_max_quality_index].reset()
-                    segments[i+1][seg2_max_quality_index].reset()
-                elif seg1_max_quality_index != -9999:
-                    segments[i][l].reset()
-                elif seg2_max_quality_index != -9999:
-                    segments[i][l].reset()
-
-                '''
-                if seg1_max_quality_index != -9999 and seg2_max_quality_index != -9999:
-                    seg1 = segments[i-1][seg1_max_quality_index]
-                    seg2 = segments[i+1][seg2_max_quality_index]
-                    if quality > seg1.quality:
-                        segments[i-1][seg1_max_quality_index].reset()
-                        if quality > seg2.quality:
-                            segments[i+1][seg2_max_quality_index].reset()
-                        else:
-                            segments[i][l].reset()
-                    else:
-                        segments[i][l].reset()
-                        if seg1.quality > seg2.quality:
-                            segments[i+1][seg2_max_quality_index].reset()
-                        else:
-                            segments[i-1][seg1_max_quality_index].reset()
-                elif seg1_max_quality_index != -9999:
-                    seg1 = segments[i-1][seg1_max_quality_index]
-                    if quality > seg1.quality:
-                        segments[i-1][seg1_max_quality_index].reset()
-                    else:
-                        segments[i][l].reset()
-                elif seg2_max_quality_index != -9999:
-                    seg2 = segments[i+1][seg2_max_quality_index]
-                    if quality > seg2.quality:
-                        segments[i+1][seg2_max_quality_index].reset()
-                    else:
-                        segments[i][l].reset()
-                '''
+        segments = cross_partition_cancellation(segments)
 
     if NUM_PARTITIONS > 8:
         segments_reduced = []
