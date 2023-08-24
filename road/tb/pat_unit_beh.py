255705d067b8d403bb6f9ab4251039a18979a6d2
Abhisek Datta <abhisek.datta@cern.ch>
2023-08-23 23:00:09 -0700

    remove large clusters to improve purity

--- road/tb/pat_unit_beh.py
+++ road/tb/pat_unit_beh.py
@@ -224,8 +224,9 @@ def pat_unit(data,
         best.reset()
     if n_layers_large_clusters > 1:
         best.reset()
-    #elif n_layers_large_clusters == 1:
-    #    ly_thresh += 1
+    #if n_layers_large_clusters == 1:
+    if n_layers_large_hits == 1:
+        ly_thresh += 1
 
     # (8) apply a layer threshold
     if (partition % 2 != 0):
