3278d4a227e9ae60707b98e668df5d742c2547cb
Abhisek Datta <abhisek.datta@cern.ch>
2024-08-27 12:02:45 -0700

    stringent cuts for high eta

--- road/tb/pat_unit_beh.py
+++ road/tb/pat_unit_beh.py
@@ -250,20 +250,14 @@ def pat_unit(data,
     for l in n_hits_counts:
         if l > n_hits_max_limit:
             n_layers_large_hits += 1
-    #if (best.id <= 12):
-    #    if n_layers_large_clusters > 0:
-    #        best.reset()
-    #elif (best.id <= 14):
-    #    if n_layers_large_clusters > 1:
-    #        best.reset()
-    #elif (best.id <= 16):
-    #    if n_layers_large_clusters > 2:
-    #        best.reset()
+    if best.partition >= 5:
+        if n_layers_large_clusters >= 1:
+            best.reset()
     #if (best.lc - n_layers_large_clusters) < 3:
-    if n_layers_large_clusters > 1:
-        best.reset()
-    if n_layers_large_hits > 0:
-        best.reset()
+    #if n_layers_large_clusters > 1:
+    #    best.reset()
+    #if n_layers_large_hits > 0:
+    #    best.reset()
 
     best.partition=partition
 
