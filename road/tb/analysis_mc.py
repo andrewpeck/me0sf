f3156f1d36d9aa19cec5fd72a10f6199037c0452
Abhisek Datta <abhisek.datta@cern.ch>
2024-09-07 23:31:44 -0700

    add fake rate histo and info

--- road/tb/analysis_mc.py
+++ road/tb/analysis_mc.py
@@ -1741,7 +1741,7 @@ def analysis(root_dat, hits, bx, bx_list, cross_part, verbose, pu, num_or):
 
     c13a = ROOT.TCanvas('', '', 800, 650)
     c13a.SetGrid()
-    c13a.DrawFrame(0, 0, 9, 20, ";#eta Partition;Nr. of Fakes per Chamber per BX")
+    c13a.DrawFrame(0, 0, 9, 0.1, ";#eta Partition;Nr. of Fakes per Chamber per BX")
     num_bkg_seg_per_chamber_per_event_eta.Scale(1/(36.0*n_total_events))
     num_bkg_seg_per_chamber_per_event_eta.Draw("same HE")
     num_bkg_seg_per_chamber_per_event_eta.SetMarkerStyle(8)
