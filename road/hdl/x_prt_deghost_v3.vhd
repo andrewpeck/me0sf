59859a4155da305c10f7cc46df04b2eded833ad6
Nick Turner <nturner1@g.ucla.edu>
2025-02-21 17:52:09 +0100

    First implementation of padding range vectors, and getting reals mask for all segs (including edges)

--- road/hdl/x_prt_deghost_v3.vhd
+++ road/hdl/x_prt_deghost_v3.vhd
@@ -61,7 +61,9 @@ architecture behavioral of x_prt_deghost_v3 is
   signal segs_padded : segment_list_t(0 to N_SEGS_PADDED_TOTAL-1);
   
   type range_vector_arr is array (0 to N_X_PRTS*N_CHUNKS_PER_PRT-1) of std_logic_vector (0 to 5);
+  type range_vector_padded_arr is array (0 to (N_X_PRTS+2)*(N_CHUNKS_PER_PRT+2)-1) of std_logic_vector (0 to 5);
   signal range_vectors : range_vector_arr;
+  signal range_vectors_padded : range_vector_padded_arr;
   
   signal mask : std_logic_vector (0 to NUM_FINDERS*N_SEGS_PRT - 1);
   signal segs_masked : segment_list_t (0 to NUM_FINDERS*N_SEGS_PRT - 1);
@@ -85,6 +87,18 @@ architecture behavioral of x_prt_deghost_v3 is
         end loop;
       end loop;
     return out_segs;
+  end function;
+  
+  -- Function to pad the 2D range vector matrix with null range vectors ("000000")
+  function pad_RV(in_RV : range_vector_arr) return range_vector_padded_arr is
+    variable out_RV : range_vector_padded_arr;
+    begin
+      for y in 0 to N_X_PRTS+2-1 loop
+        for x in 0 to N_SEGS_PRT+2-1 loop
+          out_RV((N_SEGS_PRT+2)*y + x) := (others => '0') when (y = 0 or y = N_X_PRTS+1 or x = 0 or x = N_SEGS_PRT+1) else in_RV(N_SEGS_PRT*(y-1) + (x-1));
+        end loop;
+      end loop;
+    return out_RV;
   end function;
 
   -- Function to determine if which of 6 neighboring real segments are close to the virtual segment
@@ -121,9 +135,9 @@ architecture behavioral of x_prt_deghost_v3 is
     return out_bits;
   end function;
   
-  function get_mask(range_vectors : range_vector_arr) return std_logic_vector is
+  function get_mask(range_vectors : range_vector_padded_arr) return std_logic_vector is
   
-    variable range_vectors_s2 : range_vector_arr;
+    variable range_vectors_s2 : range_vector_padded_arr;
   
     variable v_kill_bits : std_logic_vector (0 to 5);
   
@@ -132,38 +146,15 @@ architecture behavioral of x_prt_deghost_v3 is
   
   begin
     --Kill real segment ghosts
-    
-    --Need to check top and bottom partitions differently, consider zero padding
-    reals_mask(0 to N_SEGS_PRT-1) := (others => '1');
-
-    for y in 1 to NUM_FINDERS-N_X_PRTS-2 loop
-      --Check leftmost segment
-      if (range_vectors(y*N_SEGS_PRT)(5) and or_reduce(range_vectors(y*N_SEGS_PRT)(0 to 2)))  then
-        reals_mask((y+1)*N_SEGS_PRT) := '0';
---        range_vectors_s2((y-1)*N_SEGS_PRT)(4) := '0';
---        range_vectors_s2((y-1)*N_SEGS_PRT+1)(3) := '0';
---        range_vectors_s2(y*N_SEGS_PRT)(1) := '0';
---        range_vectors_s2(y*N_SEGS_PRT+1)(0) := '0';
-      else
-        reals_mask((y+1)*N_SEGS_PRT) := '1';
---        range_vectors_s2((y-1)*N_SEGS_PRT)(4) := '0';
---        range_vectors_s2((y-1)*N_SEGS_PRT+1)(3) := '0';
---        range_vectors_s2(y*N_SEGS_PRT)(1) := '0';
---        range_vectors_s2(y*N_SEGS_PRT+1)(0) := '0';
-      end if;
-
-      --Rightmost segment
-      reals_mask((y+1)*N_SEGS_PRT) := '0' when ( range_vectors(y*N_SEGS_PRT-2)(5) or range_vectors(y*N_SEGS_PRT-1)(4) ) and ( range_vectors((y+1)*N_SEGS_PRT-2)(2) or range_vectors((y+1)*N_SEGS_PRT-1)(1) ) else '1';
-      
-      --Middle segments
-      for x in 1 to N_SEGS_PRT-2 loop
-        v_kill_bits(0) := range_vectors((y-1)*N_SEGS_PRT + x - 1)(5) and or_reduce(range_vectors((y-1)*N_SEGS_PRT + x - 1)(0 to 2));
-        v_kill_bits(1) := range_vectors((y-1)*N_SEGS_PRT + x)(4) and or_reduce(range_vectors((y-1)*N_SEGS_PRT + x)(0 to 2));
-        v_kill_bits(2) := range_vectors((y-1)*N_SEGS_PRT + x + 1)(3) and or_reduce(range_vectors((y-1)*N_SEGS_PRT + x + 1)(0 to 2));
+    for y in 0 to NUM_FINDERS-N_X_PRTS-1 loop
+      for x in 0 to N_SEGS_PRT-1 loop
+        v_kill_bits(0) := range_vectors(y*N_SEGS_PRT + x)(5) and or_reduce(range_vectors(y*N_SEGS_PRT + x)(0 to 2));
+        v_kill_bits(1) := range_vectors(y*N_SEGS_PRT + x + 1)(4) and or_reduce(range_vectors(y*N_SEGS_PRT + x + 1)(0 to 2));
+        v_kill_bits(2) := range_vectors(y*N_SEGS_PRT + x + 2)(3) and or_reduce(range_vectors(y*N_SEGS_PRT + x + 2)(0 to 2));
         
-        v_kill_bits(3) := range_vectors(y*N_SEGS_PRT + x - 1)(2) and or_reduce(range_vectors(y*N_SEGS_PRT + x - 1)(3 to 5));
-        v_kill_bits(4) := range_vectors(y*N_SEGS_PRT + x)(1) and or_reduce(range_vectors(y*N_SEGS_PRT + x)(3 to 5));
-        v_kill_bits(5) := range_vectors(y*N_SEGS_PRT + x + 1)(0) and or_reduce(range_vectors(y*N_SEGS_PRT + x + 1)(3 to 5));
+        v_kill_bits(3) := range_vectors((y+1)*N_SEGS_PRT + x)(2) and or_reduce(range_vectors((y+1)*N_SEGS_PRT + x)(3 to 5));
+        v_kill_bits(4) := range_vectors((y+1)*N_SEGS_PRT + x + 1)(1) and or_reduce(range_vectors((y+1)*N_SEGS_PRT + x + 1)(3 to 5));
+        v_kill_bits(5) := range_vectors((y+1)*N_SEGS_PRT + x + 2)(0) and or_reduce(range_vectors((y+1)*N_SEGS_PRT + x + 2)(3 to 5));
         
         reals_mask(y*N_SEGS_PRT + x) := '0' when or_reduce(v_kill_bits) else '1';
       end loop;
@@ -200,8 +191,10 @@ begin
     end generate;
   end generate;
   
+  range_vectors_padded <= pad_RV(range_vectors);
+  
   --Mask
-  mask <= get_mask(range_vectors);
+  mask <= get_mask(range_vectors_padded);
   part_masking : for y in 0 to NUM_FINDERS-1 generate
     seg_masking : for x in 0 to N_SEGS_PRT-1 generate
       segs_masked(y*N_SEGS_PRT + x).id <= segs_i(y*N_SEGS_PRT + x).id;
