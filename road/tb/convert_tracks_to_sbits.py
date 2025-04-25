5c082bbcc7acb8dde758287dd63bed63ce722871
nturner0 <nturner1@g.ucla.edu>
2025-04-25 11:57:27 +0200

    Added functions for converting tracks from the QC8 'offline algorithm' back to sbits, for comparison against the FW segment finder

---
+++ road/tb/convert_tracks_to_sbits.py
@@ -0,0 +1,138 @@
+import uproot
+import sys
+
+from printly_dat import printly_dat
+import get_sbits_from_root as sfr
+
+def read_stack_tracks(file_path, entry_start=None, entry_stop=None):
+    with uproot.open(file_path + ":trackTree") as tracks:
+
+        # print("read all: ", tracks.keys())
+        
+        events = tracks.arrays(
+            [
+                'orbitNumber',
+                'bunchCounter',
+                'eventCounter',
+                'rechitChamber',
+                'rechitEta',
+                'rechitLocalX',
+                'rechitLocalY',
+                'rechitR',
+                'rechitPhi',
+                'rechitErrorX',
+                'rechitErrorY',
+                'rechitGlobalX',
+                'rechitGlobalY',
+                'rechitErrorGlobalX',
+                'rechitErrorGlobalY',
+                'rechitClusterSize',
+                'rechitClusterSizeX',
+                'rechitClusterSizeY',
+                'rechitCharge',
+                'rechitTime',
+                'partialTrackChamber',
+                'partialTrackChi2',
+                'partialTrackNHits',
+                'partialTrackAverageTime',
+                'partialTrackMedianTime',
+                'partialTrackSlopeX',
+                'partialTrackSlopeY',
+                'partialTrackInterceptX',
+                'partialTrackInterceptY',
+                'partialTrackSlopeVarianceX',
+                'partialTrackSlopeVarianceY',
+                'partialTrackInterceptVarianceX',
+                'partialTrackInterceptVarianceY',
+                'partialTrackCovarianceX',
+                'partialTrackCovarianceY',
+                'partialProphitEta',
+                'partialProphitGlobalX',
+                'partialProphitGlobalY',
+                'partialProphitErrorX',
+                'partialProphitErrorY',
+                'partialProphitLocalX',
+                'partialProphitLocalY',
+                'partialProphitR',
+                'partialProphitPhi',
+                'trackChi2',
+                'trackNHits',
+                'trackAverageTime',
+                'trackMedianTime',
+                'trackSlopeX',
+                'trackSlopeY',
+                'trackInterceptX',
+                'trackInterceptY',
+                'trackSlopeVarianceX',
+                'trackSlopeVarianceY',
+                'trackInterceptVarianceX',
+                'trackInterceptVarianceY',
+                'trackCovarianceX',
+                'trackCovarianceY',
+                'allChi2',
+                'prophitChamber',
+                'prophitEta',
+                'prophitGlobalX',
+                'prophitGlobalY',
+                'prophitErrorX',
+                'prophitErrorY',
+                'prophitLocalX',
+                'prophitLocalY',
+                'prophitR',
+                'prophitPhi'
+            ], entry_start=entry_start, entry_stop=entry_stop
+        )
+
+        return events
+
+BL = 574.8
+BS = 235.2
+H = 787.9
+LY_GAP = 35 # mm/ly
+
+def get_width_from_y(y):
+    return BL - (y*(BL-BS))/H
+
+def get_sbits_from_track(x0, y0, x_slope, y_slope):
+        y0_shift_mm = H/2.0 - y0 
+        y_proj_mm = [y0_shift_mm - ly*LY_GAP*y_slope for ly in range(6)]
+
+        y_proj_eta = [round(y_proj_mm[ly]*8.0/H - 0.5) for ly in range(6)]
+        print(f"Projected Etas: {y_proj_eta}")
+
+        x_widths = [get_width_from_y(y_proj_mm[ly]) for ly in range(6)]
+
+        x0_shift_mm = x_widths[0]/2.0 - x0
+        x_proj_mm = [x0_shift_mm - ly*LY_GAP*x_slope for ly in range(6)]
+
+        x_proj_strip = [round(x_proj_mm[ly]*191/x_widths[ly]) for ly in range(6)]
+        print(f"Projected Strips: {x_proj_strip}")
+ 
+        return (x_proj_strip, y_proj_eta)
+
+if __name__ == "__main__":
+    
+    tracks = read_stack_tracks("stack_testdata_tracks.root")
+
+    params = ("trackInterceptX", "trackInterceptY", "trackSlopeX", "trackSlopeY")
+
+    sbits_root = sfr.read_ntuple_stack("00001199.root")
+
+    for i in range(11, 14):
+        print(f"\n\n\nEVENT {i}")
+        
+        sbits_ev = sfr.get_sbits_from_event(sbits_root[i])
+
+        for param in params:
+            print(param + ": " + str(tracks[i][param]))
+
+        hits_strip, hits_eta = get_sbits_from_track(tracks[i]["trackInterceptX"], tracks[i]["trackInterceptY"], tracks[i]["trackSlopeX"], tracks[i]["trackSlopeY"])
+
+        hits_display = [[0 for _ in range(6)] for _ in range (8)]
+        for ly in range(6):
+            if not (hits_strip[ly] > 191 or hits_strip[ly] < 0):
+                hits_display[hits_eta[ly]][ly] = 2**(191-hits_strip[ly])
+
+        for prt in range(8):
+            print(f"Partition: {prt}")
+            printly_dat(mask=hits_display[prt], data=sbits_ev[prt], MAX_SPAN=192)
--- road/tb/get_sbits_from_root.py
+++ road/tb/get_sbits_from_root.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     
     segs = read_ntuple_stack("00001199.root")
 
-    for i in range(1, 2):
+    for i in range(6, 7):
         print(f"\n\n\nEVENT {i}")
         hit_data = get_sbits_from_event(segs[i])
         
