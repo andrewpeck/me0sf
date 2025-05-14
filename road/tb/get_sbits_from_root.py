232bf01675b5ef4c0aebfd5b687601b946a6c391
nturner0 <nturner1@g.ucla.edu>
2025-05-14 14:06:37 +0200

    Changed filename to convention

--- road/tb/get_sbits_from_root.py
+++ road/tb/get_sbits_from_root.py
@@ -50,7 +50,7 @@ def get_sbits_from_event(event):
 
 if __name__ == "__main__":
     
-    segs = read_ntuple_stack("00001199.root")
+    segs = read_ntuple_stack("digi_1199.root")
 
     for i in range(6, 7):
         print(f"\n\n\nEVENT {i}")
