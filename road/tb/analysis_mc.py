1814cecd8d8488c891ce45bc02a8d19e902bad06
Nick Turner <nturner1@g.ucla.edu>
2025-07-17 03:24:38 -0700

    Small change, now dynamic bx_offset_0_index

--- road/tb/analysis_mc.py
+++ road/tb/analysis_mc.py
@@ -333,8 +333,8 @@ def analysis(root_dat, hits, bx, bx_list, cross_part, verbose, pu, num_or):
     # Determines how many and which BX offsets to look at
     # bx_offset_windows = list(range(-4, 3))# if config.peaking_enabled else [0]
     bx_offset_windows = list(range(-2, 4))
-    # bx_offset_0_index = bx_offset_windows.index(0) if config.peaking_enabled else bx_offset_windows.index(0)
-    bx_offset_0_index = bx_offset_windows.index(1)
+    bx_offset_0_index = bx_offset_windows.index(1) if config.peaking_enabled else bx_offset_windows.index(0)
+    # bx_offset_0_index = bx_offset_windows.index(0)
 
     # Counters for time resolution
     n_segs_matched_by_bx = [0]*len(bx_offset_windows)
